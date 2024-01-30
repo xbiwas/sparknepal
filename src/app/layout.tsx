50fbbc6b0663194be551afa8e4a241199d7cb489
xbiwas <biwas2059@gmail.com>
2024-01-30 23:53:38 +0545

    landing page ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { dark } from "@clerk/themes";
 import "./globals.css";
+import Navigation from "@/components/site/navigation";
+import { ThemeProvider } from "@/providers/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +20,18 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider appearance={{ baseTheme: dark }}>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
+      <html lang="en" suppressHydrationWarning>
+        <body className={inter.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Navigation />
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
---
+++ src/components/site/navigation/index.tsx
@@ -0,0 +1,45 @@
+import { ModeToggle } from "@/components/global/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
+import { User } from "@clerk/nextjs/server";
+import Image from "next/image";
+import Link from "next/link";
+
+type Props = {
+  user?: null | User;
+};
+const Navigation = ({ user }: Props) => {
+  return (
+    <div className="p-4 flex items-center justify-between relative">
+      <aside className="flex items-center gap-2">
+        <Image
+          src={"/assets/plura-logo.svg"}
+          width={40}
+          height={40}
+          alt="log"
+          priority
+        />
+        <span className="text-xl font-bold">SparkNp</span>
+      </aside>
+      <nav className="hidden md:block absolute left-[50%] transform top-[50%] translate-x-[-50%] translate-y-[-50%]">
+        <ul className="flex items-center justify-center gap-8">
+          <Link href={"#"}>Pricing</Link>
+          <Link href={"#"}>About</Link>
+          <Link href={"#"}>Documentation</Link>
+          <Link href={"#"}>Features</Link>
+        </ul>
+      </nav>
+
+      <aside className="flex gap-2 items-center">
+        <Link
+          href={"/agency"}
+          className="bg-primary text-white p-2 px-4 rounded-md hover:bg-primary/80"
+        >
+          Login
+        </Link>
+        <UserButton />
+        <ModeToggle />
+      </aside>
+    </div>
+  );
+};
+export default Navigation;
